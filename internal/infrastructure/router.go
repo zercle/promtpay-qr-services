7968d1b0872468a4b873b49ff0e701d145de46a0
Kawin Viriyaprasopsook <kawin.vir@zercle.tech>
2023-01-29 23:54:49 +0700

    🚚/api/qr/v1

--- internal/infrastructure/router.go
+++ internal/infrastructure/router.go
@@ -3,7 +3,6 @@ package infrastructure
 import (
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/swagger"
-	"github.com/zercle/promtpay-qr-services/internal/handlers"
 	"github.com/zercle/promtpay-qr-services/pkg/qrpayment"
 
 	_ "github.com/zercle/promtpay-qr-services/assets/docs"
@@ -12,17 +11,11 @@ import (
 // SetupRoutes is the Router for GoFiber App
 func (s *Server) SetupRoutes(app *fiber.App) {
 
-	// API routes group
-	groupApiV1 := app.Group("/api/v:version?", handlers.ApiLimiter)
-	{
-		groupApiV1.Get("/", handlers.Index())
-	}
-
 	// App Services
 	qrpaymentUsecase := qrpayment.InitQrPaymentUsecase()
 
 	// App Routes
-	qrpayment.NewQrPaymentHandler(app.Group("/api/v1/qr"), qrpaymentUsecase)
+	qrpayment.NewQrPaymentHandler(app.Group("/api/qr/v1"), qrpaymentUsecase)
 
 	app.Get("/*", swagger.HandlerDefault)
 }
