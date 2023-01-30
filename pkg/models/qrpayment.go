648a796fa6bb03cfa02bd1d803375e95d2313a39
Kawin Viriyaprasopsook <kawin.vir@zercle.tech>
2023-01-30 14:11:05 +0700

    👔support QueryParser

--- pkg/models/qrpayment.go
+++ pkg/models/qrpayment.go
@@ -3,15 +3,15 @@ package models
 import "encoding/json"
 
 type QrRequest struct {
-	QrType       string  `form:"qr_type" json:"qr_type" validate:"optional" enums:"static,dynamic"`
-	TaxId        string  `form:"tax_id" json:"tax_id" validate:"required" minLength:"10" maxLength:"13"`
-	Suffix       string  `form:"suffix" json:"suffix" validate:"optional" maxLength:"2"`
-	Ref1         string  `form:"ref1" json:"ref1" validate:"required" maxLength:"20"`
-	Ref2         string  `form:"ref2" json:"ref2" validate:"optional" maxLength:"20"`
-	Ref3         string  `form:"ref3" json:"ref3" validate:"optional" maxLength:"20"`
-	Amount       float64 `form:"amount" json:"amount,omitempty" validate:"required" minimum:"0" maximum:"99999999999.99"`
-	MerchantName string  `form:"merchan_name" json:"merchan_name,omitempty" validate:"optional" maxLength:"20"`
-	Invoice      string  `form:"invoice" json:"invoice" validate:"optional" maxLength:"20"`
+	QrType       string  `form:"qr_type" query:"qr_type" json:"qr_type" validate:"optional" enums:"static,dynamic"`
+	TaxId        string  `form:"tax_id" query:"tax_id" json:"tax_id" validate:"required" minLength:"10" maxLength:"13"`
+	Suffix       string  `form:"suffix" query:"suffix" json:"suffix" validate:"optional" maxLength:"2"`
+	Ref1         string  `form:"ref1" query:"ref1" json:"ref1" validate:"required" maxLength:"20"`
+	Ref2         string  `form:"ref2" query:"ref2" json:"ref2" validate:"optional" maxLength:"20"`
+	Ref3         string  `form:"ref3" query:"ref3" json:"ref3" validate:"optional" maxLength:"20"`
+	Amount       float64 `form:"amount" query:"amount" json:"amount,omitempty" validate:"required" minimum:"0" maximum:"99999999999.99"`
+	MerchantName string  `form:"merchan_name" query:"merchan_name" json:"merchan_name,omitempty" validate:"optional" maxLength:"20"`
+	Invoice      string  `form:"invoice" query:"invoice" json:"invoice" validate:"optional" maxLength:"20"`
 }
 
 type QrResponse struct {
